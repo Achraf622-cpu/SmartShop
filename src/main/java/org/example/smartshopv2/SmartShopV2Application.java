4de076ee14144eef45e5ce4405dcae2b33f17281
Achraf Hanzaz <hanzazachraf581@gmail.com>
2025-11-25 14:47:12 +0100

    initialisation du projet et UML

---
+++ src/main/java/org/example/smartshopv2/SmartShopV2Application.java
@@ -0,0 +1,13 @@
+package org.example.smartshopv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartShopV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartShopV2Application.class, args);
+	}
+
+}
