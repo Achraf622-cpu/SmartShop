a2c96b3504d96f3e923dc400f6130e5fb9c1b8ce
Achraf Hanzaz <hanzazachraf581@gmail.com>
2025-12-07 23:32:32 +0100

    SMAR-37: #in-progress Exception du business

---
+++ src/main/java/org/example/smartshopv2/exception/BusinessRuleException.java
@@ -0,0 +1,7 @@
+package org.example.smartshopv2.exception;
+
+public class BusinessRuleException extends RuntimeException {
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+}
